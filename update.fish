d21136eff40a0492cd6b775dbabf2a43c5ce4184
Trần Đức Nam <dynamo.foss@gmail.com>
2023-02-19 20:34:34 +0700

    feat: add update script of asdf, bat, dog, exa

---
+++ update.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+curl -SL https://raw.githubusercontent.com/asdf-vm/asdf/master/completions/asdf.fish -o completions/asdf.fish
+curl -SL https://raw.githubusercontent.com/sharkdp/bat/master/assets/completions/bat.fish.in -o completions/bat.fish
+curl -SL https://raw.githubusercontent.com/ogham/dog/master/completions/dog.fish -o completions/dog.fish
+curl -SL https://raw.githubusercontent.com/ogham/exa/master/completions/fish/exa.fish -o completions/exa.fish
