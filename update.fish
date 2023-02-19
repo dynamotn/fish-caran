d3744343dc3f7533eb0e379625e8920647c8a684
Trần Đức Nam <dynamo.foss@gmail.com>
2023-02-19 20:38:00 +0700

    refactor: move update script to command of fish shell

--- update.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-curl -SL https://raw.githubusercontent.com/asdf-vm/asdf/master/completions/asdf.fish -o completions/asdf.fish
-curl -SL https://raw.githubusercontent.com/sharkdp/bat/master/assets/completions/bat.fish.in -o completions/bat.fish
-curl -SL https://raw.githubusercontent.com/ogham/dog/master/completions/dog.fish -o completions/dog.fish
-# TODO: docker-compose
-curl -SL https://raw.githubusercontent.com/ogham/exa/master/completions/fish/exa.fish -o completions/exa.fish
-# TODO: fd
