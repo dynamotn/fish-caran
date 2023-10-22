92bbef5caf3f69f3aa2f7318be11338d8410b228
Trần Đức Nam <dynamo.foss@gmail.com>
2023-10-22 12:46:00 +0700

    feat: update completions of some tools

--- .development/update.fish
+++ .development/update.fish
@@ -3,10 +3,6 @@
 curl -SL https://raw.githubusercontent.com/asdf-vm/asdf/master/completions/asdf.fish -o completions/asdf.fish
 curl -SL https://raw.githubusercontent.com/sharkdp/bat/master/assets/completions/bat.fish.in -o completions/bat.fish
 curl -SL https://raw.githubusercontent.com/twpayne/chezmoi/master/completions/chezmoi.fish -o completions/chezmoi.fish
-curl -SL https://raw.githubusercontent.com/ogham/dog/master/completions/dog.fish -o completions/dog.fish
-# TODO: docker-compose
-curl -SL https://raw.githubusercontent.com/ogham/exa/master/completions/fish/exa.fish -o completions/exa.fish
-# TODO: fd
-# TODO: hyperfine
+curl -SL https://raw.githubusercontent.com/dynamotn/dog/master/completions/dog.fish -o completions/dog.fish
+curl -SL https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish -o completions/eza.fish
 command -v kubectl && kubectl completion fish > completions/kubectl.fish
-# TODO: rg
