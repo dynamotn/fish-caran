fd564b2197635e11ec121d0891664fc5ee4773a6
Trần Đức Nam <dynamo.foss@gmail.com>
2023-10-23 14:27:33 +0700

    refactor: change alias of chezmoi

--- conf.d/chezmoi.fish
+++ conf.d/chezmoi.fish
@@ -1,7 +1,17 @@
 alias cz 'chezmoi'
-alias cza 'cz add'
-alias czA 'cz apply'
-alias cze 'cz encrypt'
-alias czd 'cz decrypt'
-alias czD 'cz diff'
+alias cza 'cz apply'
+alias czA 'cz add'
+alias cze 'cz edit'
+alias czd 'cz diff'
 alias czu 'cz update'
+
+function scz --wraps 'chezmoi' --description 'chezmoi with sudo'
+  set -l dotfiles (realpath $(chezmoi execute-template '{{ .chezmoi.sourceDir }}')/..)
+  sudo chezmoi --destination / --source "$dotfiles/root" --working-tree "$dotfiles" --config ~/.config/chezmoi/chezmoi.yaml $argv
+end
+
+alias scza 'scz apply'
+alias sczA 'scz add'
+alias scze 'scz edit'
+alias sczd 'scz diff'
+alias sczu 'scz update'
