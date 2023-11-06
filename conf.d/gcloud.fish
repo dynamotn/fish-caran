8db4f081430fde29b0792cc7dd8c0f53f27d6b5a
Trần Đức Nam <dynamo.foss@gmail.com>
2023-11-06 10:36:38 +0700

    feat: add some alias of gcloud

--- conf.d/gcloud.fish
+++ conf.d/gcloud.fish
@@ -1,12 +1,16 @@
 alias G "gcloud"
+alias Gal "gcloud auth login"
+alias Gb "bq query --use_legacy_sql=false"
 alias Gc "gcloud compute"
+alias Gca "gcloud compute addresses"
 alias Gci "gcloud compute instances"
 alias Gcf "gcloud compute firewall-rules"
+alias Gi "gcloud iam"
 alias Gk "gcloud container"
+alias Gl "gcloud logging"
 alias Gp "gcloud pubsub"
 alias Gs "gcloud sql"
-alias Gi "gcloud iam"
-alias Gl "gcloud logging"
+alias Gu "gcloud components update"
 
 alias fGpc "__caran_fzf_gcloud_project_change"
 alias fGcis "__caran_fzf_gcloud_instance_ssh"
@@ -16,3 +20,8 @@ set -q FZF_GCLOUD_PROJECT_SWITCH_COMMAND; or set -U FZF_GCLOUD_PROJECT_SWITCH_CO
 set -q FZF_GCLOUD_PROJECT_SEARCH_COMMAND; or set -U FZF_GCLOUD_PROJECT_SEARCH_COMMAND "gcloud projects list --format='table[no-heading](projectId,name)'"
 set -q FZF_GCLOUD_INSTANCE_SEARCH_COMMAND; or set -U FZF_GCLOUD_INSTANCE_SEARCH_COMMAND "gcloud compute instances list | tail -n +2"
 set -q FZF_GCLOUD_ACTIVE_INSTANCE_SEARCH_COMMAND; or set -U FZF_GCLOUD_ACTIVE_INSTANCE_SEARCH_COMMAND "gcloud compute instances list --filter=\"status=RUNNING\" | tail -n +2"
+
+function Gcs
+  set -l gcs_bucket (string replace "gs://" "" $argv[1])
+  xdg-open https://console.cloud.google.com/storage/browser/$gcs_bucket
+end
