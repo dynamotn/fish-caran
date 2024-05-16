8c898065d19aeb2a8d70e60ff3f0ec1818063c3d
Trần Đức Nam <me@dynamotn.dev>
2024-05-17 00:05:00 +0700

    feat: copy paste on wayland

--- conf.d/clipboard.fish
+++ conf.d/clipboard.fish
@@ -1,2 +1,7 @@
-alias copy 'xsel -i -b'
-alias paste 'xsel -o -b'
+if test $XDG_SESSION_TYPE = "wayland"
+  alias copy 'wl-copy'
+  alias paste 'wl-paste'
+else
+  alias copy 'xsel -i -b'
+  alias paste 'xsel -o -b'
+end
