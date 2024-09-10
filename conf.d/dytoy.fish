684f7ac69ce1134403fe7726f58b4e60559faf79
Trần Đức Nam <me@dynamotn.dev>
2024-09-10 12:13:40 +0700

    feat: add dytoy

---
+++ conf.d/dytoy.fish
@@ -0,0 +1,14 @@
+function dytoy_binary
+  ~/.config/dytoy/binary.sh
+end
+function dytoy_os
+  ~/.config/dytoy/os.sh
+end
+function dytoy_shell
+  ~/.config/dytoy/shell.sh
+end
+function dytoy_all
+  dytoy_os
+  dytoy_binary
+  dytoy_shell
+end
