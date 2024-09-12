2b8a3e1bfd71bf45ba194007f1f1ad7085296155
Trần Đức Nam <me@dynamotn.dev>
2024-09-12 15:08:28 +0700

    fix: add parameters for dytoy commands

--- conf.d/dytoy.fish
+++ conf.d/dytoy.fish
@@ -1,14 +1,14 @@
 function dytoy_binary
-  ~/.config/dytoy/binary.sh
+  ~/.config/dytoy/binary.sh $argv
 end
 function dytoy_os
-  ~/.config/dytoy/os.sh
+  ~/.config/dytoy/os.sh $argv
 end
 function dytoy_shell
-  ~/.config/dytoy/shell.sh
+  ~/.config/dytoy/shell.sh $argv
 end
 function dytoy_all
-  dytoy_os
-  dytoy_binary
-  dytoy_shell
+  dytoy_os -e
+  dytoy_binary -e -n
+  dytoy_shell -e
 end
