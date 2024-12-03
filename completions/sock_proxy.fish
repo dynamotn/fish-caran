9012f0e0e1fb90aae0643b7343e01a9a291fa8f7
Trần Đức Nam <me@dynamotn.dev>
2024-12-03 17:24:36 +0700

    temp

--- completions/sock_proxy.fish
+++ completions/sock_proxy.fish
@@ -1,5 +1,5 @@
 complete -xc sock_proxy -d "Hostname" --arguments "(__fish_print_hostnames)"
-__dynamo_print_common_port | while read -l description port
-  complete -xc sock_proxy -s p -d $description --arguments $port --condition __dynamo_need_argument
+__caran_print_common_port | while read -l description port
+  complete -xc sock_proxy -s p -d $description --arguments $port --condition __caran_need_ssh_argument
 end
 complete -fc sock_proxy -s p -d "Port"
