e4d8373daec58437bc0ecd1a0a8d753f06db3a51
Trần Đức Nam <me@dynamotn.dev>
2024-12-04 14:19:17 +0700

    refactor: remove kubectl completions

--- .development/update.fish
+++ .development/update.fish
@@ -4,4 +4,3 @@ curl -SL https://raw.githubusercontent.com/asdf-vm/asdf/master/completions/asdf.
 curl -SL https://raw.githubusercontent.com/sharkdp/bat/master/assets/completions/bat.fish.in -o completions/bat.fish
 curl -SL https://raw.githubusercontent.com/twpayne/chezmoi/master/completions/chezmoi.fish -o completions/chezmoi.fish
 curl -SL https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish -o completions/eza.fish
-command -v kubectl && kubectl completion fish > completions/kubectl.fish
