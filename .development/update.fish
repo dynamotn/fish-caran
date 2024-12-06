1ed6f0f5eea5cd97ce65b4bffbe8af6d0ec41e19
Trần Đức Nam <me@dynamotn.dev>
2024-12-06 17:40:43 +0700

    refactor: remove completions of supported generation tool

--- .development/update.fish
+++ .development/update.fish
@@ -1,6 +1,4 @@
 #!/usr/bin/env fish
 
-curl -SL https://raw.githubusercontent.com/asdf-vm/asdf/master/completions/asdf.fish -o completions/asdf.fish
-curl -SL https://raw.githubusercontent.com/sharkdp/bat/master/assets/completions/bat.fish.in -o completions/bat.fish
-curl -SL https://raw.githubusercontent.com/twpayne/chezmoi/master/completions/chezmoi.fish -o completions/chezmoi.fish
 curl -SL https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish -o completions/eza.fish
+curl -SL https://raw.githubusercontent.com/brgmnn/fish-docker-compose/refs/heads/master/completions/docker-compose.fish -o completions/docker-compose.fish
