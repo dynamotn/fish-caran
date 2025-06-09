32edef28a3a32d493ab31a0c9778e816be850d74
Trần Đức Nam <me@dynamotn.dev>
2025-06-09 16:01:42 +0700

    refactor: remove `scz` to use executable file

--- functions/scz.fish
+++
@@ -1,9 +0,0 @@
-function scz --wraps 'chezmoi' --description 'chezmoi with sudo'
-  set -l dotfiles (realpath (chezmoi execute-template '{{ .chezmoi.sourceDir }}')/..)
-  sudo env "PATH=$PATH" \
-    chezmoi --destination / \
-    --source "$dotfiles/root" \
-    --working-tree "$dotfiles" \
-    --config ~/.config/chezmoi/root_chezmoi.yaml \
-    $argv
-end
