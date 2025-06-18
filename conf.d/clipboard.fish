ba0d8bd5327d47117a75e237482c68e8f96b57fc
Trần Đức Nam <me@dynamotn.dev>
2025-06-18 12:47:06 +0700

    feat: clipboard on MacOS

--- conf.d/clipboard.fish
+++ conf.d/clipboard.fish
@@ -1,7 +1,10 @@
-if test "$XDG_SESSION_TYPE" = "wayland"
-  alias copy 'wl-copy'
-  alias paste 'wl-paste'
+if type -q sw_vers
+    alias copy 'pbcopy'
+    alias copy 'pbpaste'
+else if test "$XDG_SESSION_TYPE" = "wayland"
+    alias copy 'wl-copy'
+    alias paste 'wl-paste'
 else
-  alias copy 'xsel -i -b'
-  alias paste 'xsel -o -b'
+    alias copy 'xsel -i -b'
+    alias paste 'xsel -o -b'
 end
