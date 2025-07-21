c97d42a0c3b3b7c9ac1cf654eda61ebf141e8d43
Trần Đức Nam <me@dynamotn.dev>
2025-07-22 00:15:12 +0700

    feat: add new completion of dytoy

--- completions/dytoy_all.fish
+++ completions/dytoy_all.fish
@@ -1,9 +1,23 @@
-complete -xc dytoy_all -s h -d "Display help"
-complete -xc dytoy_all -s n -d "Update only not existed tool"
-complete -xc dytoy_all -s e -d "Update only essential tool"
+complete -xc dytoy_all -s l -d "Set log level"
+complete -xc dytoy_all -l log-level -d "Set log level"
+for log_level in trace debug info warn error fatal
+    complete -xc dytoy_all -s l --arguments $log_level
+    complete -xc dytoy_all -l log-level --arguments $log_level
+end
+complete -xc dytoy_all -s D -d "Dry run"
+complete -xc dytoy_all -l dry-run -d "Dry run"
 complete -xc dytoy_all -s t -d "Only one specific tool"
+complete -xc dytoy_all -l tool -d "Only one specific tool"
+complete -xc dytoy_all -s e -d "Install only essential tool"
+complete -xc dytoy_all -l essential -d "Install only essential tool"
+complete -xc dytoy_all -s i -d "Install only not installed tool"
+complete -xc dytoy_all -l check-installed -d "Install only not installed tool"
+complete -xc dytoy_all -l no-check-installed -d "Allow to reinstall tool"
 type -q yq; and yq e -r -o=j -I=0 '.[].name' \
     ~/.config/dytoy/tools.yaml | while read -l tool
     complete -xc dytoy_all -s t --arguments $tool
 end
-complete -xc dytoy_all -s d -d "Debug mode"
+complete -xc dytoy_all -s L -d "List files in compressed instead of extract"
+complete -xc dytoy_all -l list -d "List files in compressed instead of extract"
+complete -xc dytoy_all -s h -d "Display help"
+complete -xc dytoy_all -l help -d "Display help"
