25f7e84a6c5ce942eb9708b8c8029110b1a8b537
Trần Đức Nam <me@dynamotn.dev>
2025-08-11 00:35:25 +0700

    feat: copy paste on termux

--- conf.d/clipboard.fish
+++ conf.d/clipboard.fish
@@ -1,6 +1,9 @@
 if type -q sw_vers
     alias copy pbcopy
-    alias copy pbpaste
+    alias paste pbpaste
+else if type -q termux-setup-storage
+    alias copy termux-clipboard-set
+    alias paste termux-clipboard-get
 else if test "$XDG_SESSION_TYPE" = wayland
     alias copy wl-copy
     alias paste wl-paste
