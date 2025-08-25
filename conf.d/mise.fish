d4d23a0ceeae88fc4c1ec69f9c0f2d7c19e94b85
Trần Đức Nam <me@dynamotn.dev>
2025-08-25 22:33:16 +0700

    style: reindent with 4 spaces

--- conf.d/mise.fish
+++ conf.d/mise.fish
@@ -1,26 +1,26 @@
-function __caran_mise_env_eval --on-event fish_prompt --description 'Update mise environment when changing directories';
-    mise hook-env -s fish | source;
+function __caran_mise_env_eval --on-event fish_prompt --description 'Update mise environment when changing directories'
+    mise hook-env -s fish | source
 
-    if test "$mise_fish_mode" != "disable_arrow";
-        function __caran_mise_cd_hook --on-variable PWD --description 'Update mise environment when changing directories';
-            if test "$mise_fish_mode" = "eval_after_arrow";
-                set -g __caran_mise_env_again 0;
-            else;
-                mise hook-env -s fish | source;
-            end;
-        end;
-    end;
-end;
+    if test "$mise_fish_mode" != disable_arrow
+        function __caran_mise_cd_hook --on-variable PWD --description 'Update mise environment when changing directories'
+            if test "$mise_fish_mode" = eval_after_arrow
+                set -g __caran_mise_env_again 0
+            else
+                mise hook-env -s fish | source
+            end
+        end
+    end
+end
 
-function __caran_mise_env_eval_2 --on-event fish_preexec --description 'Update mise environment when changing directories';
-    if set -q __caran_mise_env_again;
-        set -e __caran_mise_env_again;
-        mise hook-env -s fish | source;
-        echo;
-    end;
+function __caran_mise_env_eval_2 --on-event fish_preexec --description 'Update mise environment when changing directories'
+    if set -q __caran_mise_env_again
+        set -e __caran_mise_env_again
+        mise hook-env -s fish | source
+        echo
+    end
 
-    functions --erase __caran_mise_cd_hook;
-end;
+    functions --erase __caran_mise_cd_hook
+end
 
 __caran_mise_env_eval
 
@@ -31,7 +31,7 @@ end
 
 function fish_command_not_found
     if string match -qrv -- '^(?:mise$|mise-)' $argv[1] &&
-        mise hook-not-found -s fish -- $argv[1]
+            mise hook-not-found -s fish -- $argv[1]
         mise hook-env -s fish | source
     else if functions -q __caran_mise_fish_command_not_found
         __caran_mise_fish_command_not_found $argv
--- functions/mise.fish
+++ functions/mise.fish
@@ -13,20 +13,20 @@ function mise
     end
 
     switch "$command"
-    case deactivate shell sh
-        # if help is requested, don't eval
-        if contains -- -h $argv
-            command mise "$command" $argv
-        else if contains -- --help $argv
-            command mise "$command" $argv
-        else
-            source (command mise "$command" $argv |psub)
-        end
-    case '*'
-        if type -q termux-setup-storage
-            proot -b "$PREFIX/etc/resolv.conf:/etc/resolv.conf" -b "$PREFIX/etc/tls:/etc/ssl" mise "$command" $argv
-        else
-            command mise "$command" $argv
-        end
+        case deactivate shell sh
+            # if help is requested, don't eval
+            if contains -- -h $argv
+                command mise "$command" $argv
+            else if contains -- --help $argv
+                command mise "$command" $argv
+            else
+                source (command mise "$command" $argv |psub)
+            end
+        case '*'
+            if type -q termux-setup-storage
+                proot -b "$PREFIX/etc/resolv.conf:/etc/resolv.conf" -b "$PREFIX/etc/tls:/etc/ssl" mise "$command" $argv
+            else
+                command mise "$command" $argv
+            end
     end
 end
