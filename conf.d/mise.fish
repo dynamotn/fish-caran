d7d4b22ca7ba11be73a2a96036bddc508e966777
Trần Đức Nam <me@dynamotn.dev>
2025-08-26 23:16:49 +0700

    fix: check has command mise before hook to event

--- conf.d/mise.fish
+++ conf.d/mise.fish
@@ -1,25 +1,29 @@
 function __caran_mise_env_eval --on-event fish_prompt --description 'Update mise environment when changing directories'
-    mise hook-env -s fish | source
+    if command -sq mise
+        command mise hook-env -s fish | source
 
-    if test "$mise_fish_mode" != disable_arrow
-        function __caran_mise_cd_hook --on-variable PWD --description 'Update mise environment when changing directories'
-            if test "$mise_fish_mode" = eval_after_arrow
-                set -g __caran_mise_env_again 0
-            else
-                mise hook-env -s fish | source
+        if test "$mise_fish_mode" != disable_arrow
+            function __caran_mise_cd_hook --on-variable PWD --description 'Update mise environment when changing directories'
+                if test "$mise_fish_mode" = eval_after_arrow
+                    set -g __caran_mise_env_again 0
+                else
+                    command mise hook-env -s fish | source
+                end
             end
         end
     end
 end
 
 function __caran_mise_env_eval_2 --on-event fish_preexec --description 'Update mise environment when changing directories'
-    if set -q __caran_mise_env_again
-        set -e __caran_mise_env_again
-        mise hook-env -s fish | source
-        echo
-    end
+    if command -sq mise
+        if set -q __caran_mise_env_again
+            set -e __caran_mise_env_again
+            command mise hook-env -s fish | source
+            echo
+        end
 
-    functions --erase __caran_mise_cd_hook
+        functions --erase __caran_mise_cd_hook
+    end
 end
 
 __caran_mise_env_eval
@@ -31,8 +35,9 @@ end
 
 function fish_command_not_found
     if string match -qrv -- '^(?:mise$|mise-)' $argv[1] &&
-            mise hook-not-found -s fish -- $argv[1]
-        mise hook-env -s fish | source
+            command -sq mise &&
+            command mise hook-not-found -s fish -- $argv[1]
+        command mise hook-env -s fish | source
     else if functions -q __caran_mise_fish_command_not_found
         __caran_mise_fish_command_not_found $argv
     else
