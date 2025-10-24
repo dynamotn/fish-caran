2f6171040460a45e11e1ab4f906d97812767ecfb
Trần Đức Nam <me@dynamotn.dev>
2025-10-24 16:22:14 +0700

    fix: don't use alias in ssh command

--- functions/__caran_ssh_config_dir.fish
+++ functions/__caran_ssh_config_dir.fish
@@ -4,11 +4,11 @@ function __caran_ssh_config_dir
     set -g ssh_config_dir $HOME/.ssh/config.d
 
     if not [ -d $ssh_config_dir ]
-        mkdir $ssh_config_dir
-        mv $ssh_config $ssh_config_dir/base.config
+        command mkdir $ssh_config_dir
+        command mv $ssh_config $ssh_config_dir/base.config
         echo "Your OpenSSH config was moved to new directory $ssh_config_dir" >&2
     end
 
-    [ -f $ssh_config ]; and rm $ssh_config
-    cat $ssh_config_dir/**.config >>$ssh_config
+    [ -f $ssh_config ]; and command rm $ssh_config
+    command cat $ssh_config_dir/**.config >>$ssh_config
 end
